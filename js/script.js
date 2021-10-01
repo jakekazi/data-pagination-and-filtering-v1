eb35a0c2a2c7fdaa38625fd5ac2b78c4ccf446ae
Jake Kazi <kazijake@gmail.com>
2021-09-30 20:15:09 -0400

    Finalized the codes

--- js/script.js
+++ js/script.js
@@ -2,37 +2,52 @@
 function showPage(list, page) {
    let startIndex = (page * 9) - 9
    let endIndex = page * 9
-   let studentList = document.getElementsByClassName('student-list')
+   let studentList = document.querySelector('.student-list')
    studentList.innerHTML = ''
 
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         let studentInfo = `<li class="student-item cf"><div class="student-details">
+         studentList.insertAdjacentHTML('beforeend', `<li class="student-item cf"><div class="student-details">
          <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
          <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
          <span class="email">${list[i].email}</span></div>
          <div class="joined-details"><span class="date">${list[i].registered.date}</span></div></li>`
-
-         studentList.insertAdjacentHTML("beforeend", studentInfo)
+         )
       }
    }
 }
 
+//This function will create and insert/append the elements needed for the pagination buttons
+function addPagination(list) {
+   let numberOfPaginationButtons = Math.ceil(list.length / 9)
+   let buttons = document.querySelector('.link-list')
+   buttons.innerHTML = ''
+
+   let i = 1
+   while (i <= numberOfPaginationButtons) {
+      buttons.insertAdjacentHTML('beforeend', `<li><button type="button">${i}</button></li>`)
+      i++
+   }
+
+   let firstButton = document.querySelector('button')
+   firstButton.className = 'active'
+
+   buttons.addEventListener('click', (e) => {
+      let buttons = document.querySelectorAll('.link-list li')
+
+      //Loop over the array of link buttons and if any button has the active class, remove it.
+      for (let i = 0; i < buttons.length; i++) {
+         if (buttons[i].firstElementChild.className === 'active') {
+            buttons[i].firstElementChild.classList.remove('active')
+         }
+      }
+
+      e.target.className = 'active'
+      //Select the number of the page that was clicked to pass to the showPage function
+      let clickedPage = parseInt(e.target.innerText)
+      showPage(list, clickedPage)
+   })
+}
+
 showPage(data, 1)
-
-// //This function will create and insert/append the elements needed for the pagination buttons
-// function addPagination(list) {
-//    let numberOfPaginationButtons = Math.ceil(list.length / 9)
-//    let buttons = document.getElementsByClassName('link-list')
-//    buttons.innerHTML = ''
-
-//    let i = 1
-//    while (i <= numberOfPaginationButtons) {
-//       let button = `<li><button type="button">${i}</button></li>`
-//       buttons.insertAdjacentHTML("beforeend", button)
-//       i++
-//    }
-//    return buttons
-// }
-
-//addPagination(data)
+addPagination(data)
